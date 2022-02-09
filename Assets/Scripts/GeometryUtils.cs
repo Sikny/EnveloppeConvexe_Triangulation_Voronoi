6dcad579514af1ae6d242e58ae8e7eeddc953698
Quentin <pamartqu@gmail.com>
2022-02-09 20:46:03 +0100

    mini clean

--- Assets/Scripts/GeometryUtils.cs
+++ Assets/Scripts/GeometryUtils.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using UnityEngine;
 
 public static class GeometryUtils {
+    private const float Tolerance = 0.0001f;
+    
+    #region CONVEX_HULL
     public static Vector3[] RunJarvisMarch(Vector3[] points) {
         // polygone resultat
         List<Vector3> polygon = new List<Vector3>();
@@ -135,4 +138,82 @@ public static class GeometryUtils {
 
         return polygon.Select(vector2 => new Vector3(vector2.x, 0, vector2.y)).ToArray();
     }
+    #endregion
+
+    #region TRIANGULATION
+
+    public static int[] RunIncrementalTriangulation(Vector3[] points)
+    {
+        // 1 - tri par abscisse croissante
+        var sorted = false;
+        var pointsCount = points.Length;
+        while (!sorted) {
+            sorted = true;
+            for (int i = 0; i < pointsCount - 1; ++i) {
+                if (points[i].x > points[i + 1].x
+                    || points[i].x > points[i + 1].x + Tolerance && points[i].y > points[i + 1].y) {
+                    sorted = false;
+                    // swap
+                    (points[i], points[i + 1]) = (points[i + 1], points[i]);
+                }
+            }
+        }
+
+        // resultat
+        var result = new List<int>();
+        int currentIndex = 0;
+        
+        // 2 - initialisation
+        // a - on construit une suite de k - 1 aretes colineaires avec les k points alignés
+        var alignedPoints = new List<Vector3>();
+        float firstX = points[0].x;
+        alignedPoints.Add(points[0]);
+        for (int i = 1; i < pointsCount; ++i) {
+            if (points[i].x - firstX < Tolerance) {
+                alignedPoints.Add(points[i]);
+            }
+            else break; // we stop on first too far point
+        }
+        // b - avec le premier point suivant à droite, trianguler
+        if (alignedPoints.Count >= 2) {
+            currentIndex = alignedPoints.Count;
+            for (int i = 0; i < currentIndex; ++i) {
+                if(result.Count == 0 || result[result.Count - 1] != currentIndex)
+                    result.Add(currentIndex);
+                result.Add(i);
+                result.Add(i+1);
+                result.Add(currentIndex);
+            }
+        }
+        else {
+            result.Add(2);
+            result.Add(0);
+            result.Add(1);
+            result.Add(2);
+            currentIndex = 3;
+        }
+        
+        // 3 - iterer sur les points restants et trianguler avec les aretes vues par chaque point
+        for (int i = currentIndex; i < pointsCount; ++i) {
+            // a - recherche des arretes vues par le point i
+            var currentPolygon = GeometryUtils.RunJarvisMarch(points.Take(i).ToArray());
+            for (int j = currentPolygon.Length - 1; j > 0; --j) {
+                Vector3 p1 = currentPolygon[j], p2 = currentPolygon[j - 1];
+                Vector3 n = Vector3.Cross((p2 - p1).normalized, Vector3.down);
+                Vector3 point = points[i];
+                float dot = Vector3.Dot((point - p1).normalized, n);
+                if(dot > 0) {
+                    // b - pour toute arrete vue, ajouter au resultat le triangle associe
+                    if(result[result.Count - 1] != i)
+                        result.Add(i);
+                    result.Add(points.ToList().IndexOf(currentPolygon[j]));
+                    result.Add(points.ToList().IndexOf(currentPolygon[j-1]));
+                    result.Add(i);
+                }
+            }
+        }
+
+        return result.ToArray();
+    }
+    #endregion
 }
